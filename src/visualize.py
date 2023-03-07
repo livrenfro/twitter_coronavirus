d604329809dc2175d6c4eec5185b5d06ae0c715b
livrenfro <orenfro23@cmc.edu>
2023-03-06 19:57:04 -0800

    removing plots

--- src/visualize.py
+++ src/visualize.py
@@ -28,7 +28,7 @@ if args.percent:
         counts[args.key][k] /= counts['_all'][k]
 
 # print the count values
-items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
+items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)[:10]
 print(items)
 print(type(items))
 print(type(items[1]))
@@ -41,7 +41,7 @@ for k,v in items:
   #  mpl.use('Agg')
 
 # sorting, taking top ten, storing in variables
-items = sorted(sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)[:10], key=lambda x: x[1])
+#items = sorted(sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)[:10], key=lambda x: x[1])
 
 x = []
 y = []
